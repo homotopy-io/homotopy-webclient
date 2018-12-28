4f973fc922661e7b3b3dd4d089bd33b8886368b0
Jamie Vicary <jamievicary@gmail.com>
2018-12-28 14:45:22 +0000

    fixed issue

--- client/src/state/store/persist.js
+++ client/src/state/store/persist.js
@@ -2,7 +2,7 @@ import * as PersistActions from "../actions/persist";
 
 export default (state, action) => {
   switch (action.type) {
-  case [PersistActions.LOADED]: {
+  case PersistActions.LOADED: {
     state = { ...state, ...action.payload };
     return state;
   }
