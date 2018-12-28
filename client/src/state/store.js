9dc0355210fac7ea0675c05e7a17fdbd70c23eeb
Jamie Vicary <jamievicary@gmail.com>
2018-12-28 14:38:24 +0000

    minor

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,15 +1,12 @@
 import workspace from "~/state/store/workspace";
 import signature from "~/state/store/signature";
 import attach from "~/state/store/attach";
-<<<<<<< HEAD
-import object_store from "~/state/store/object_store";
-import LZ from "lz-string";
-import * as Core from "homotopy-core";
-import dotProp from "dot-prop-immutable";
+//import object_store from "~/state/store/object_store";
+//import LZ from "lz-string";
+//import * as Core from "homotopy-core";
+//import dotProp from "dot-prop-immutable";
 //import stringify from "json-stringify-safe";
-=======
 import persist from "~/state/store/persist";
->>>>>>> a3e435bc431b9b7145e1ce7f86f4c47943e6614a
 
 export const initialState = {
   signature: {
