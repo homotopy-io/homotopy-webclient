37bb6a87b17cd271d58ad92a3ef44bedb197bdaf
Jamie Vicary <jamievicary@gmail.com>
2018-12-28 13:06:21 +0000

    updated

--- client/src/state/store/object_store.js
+++ client/src/state/store/object_store.js
@@ -12,22 +12,6 @@ export default function reduce_object_store(state, action) {
   if (action.type.indexOf('INIT') >= 0) return state;
   if (action.type === 'persist/PERSIST') return state;
 
-  if (action.type === 'persist/REHYDRATE') {
-    if (!action.payload) return state;
-    let sc_json = action.payload.object_store.serialize_cyclic;
-    if (sc_json === null) return state;
-    let head = sc_json.head;
-    let entries = sc_json.entries;
-    let index_to_stored_array = sc_json.index_to_stored_array;
-    let sc = Core.SerializeCyclic.fromJSON({ head, entries, index_to_stored_array });
-    let new_state = sc.getHead();
-    state = dotProp.set(state, `signature`, new_state.signature);
-    state = dotProp.set(state, `workspace`, new_state.workspace);
-    state = dotProp.set(state, `attach`, new_state.attach);
-    state = dotProp.set(state, `object_store.serialize_cyclic`, sc);
-    return state;
-  }
-
   // Otherwise, update the object store
   if (!state.object_store.serialize_cyclic) state.object_store.serialize_cyclic = new Core.SerializeCyclic();
   let sc = state.object_store.serialize_cyclic;
