1308e89ab831b49f7dbaa0c5583982085d1736ae
Jamie Vicary <jamievicary@gmail.com>
2019-01-14 22:04:40 +0000

    button updates

--- client/src/components/tools/Buttons.js
+++ client/src/components/tools/Buttons.js
@@ -1,10 +1,17 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import MaterialButton from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Tool, { Control } from "~/components/Tool";
-import styled from "styled-components";
-import { getSource, getTarget, getDiagram } from "~/state/store/workspace";
+import { getSource, getTarget, getDiagram, getSlice } from "~/state/store/workspace";
+
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 const ActionButtonStyle = {
   margin: "5px",
@@ -17,18 +24,20 @@ const ActionToolStyle = {
 
 export const ButtonTool = ({
   onSource, onTarget, onIdentity, onClear, onRestrict, onTheorem, onBehead,
-  source, target, diagram
+  source, target, diagram, slice
 }) => {
   return (
     <Tool title="Actions" style={ActionToolStyle}>
       <div style={{maxWidth: '350px', padding: '5px'}}>
-      <MaterialButton disabled={!diagram || (target && !diagram.sameBoundary(target))} style={ActionButtonStyle} variant="contained" onClick={onSource}> <u>S</u>ource </MaterialButton>
-      <MaterialButton disabled={!diagram || (source && !diagram.sameBoundary(source))} style={ActionButtonStyle} variant="contained" onClick={onTarget}> <u>T</u>arget </MaterialButton>
-      <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onIdentity}> <u>I</u>dentity </MaterialButton>
-      <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onClear}> <u>C</u>lear </MaterialButton>
-      <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onRestrict}> <u>R</u>estrict </MaterialButton>
-      <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onTheorem}> T<u>h</u>eorem </MaterialButton>
-      <MaterialButton disabled={!diagram || diagram.n == 0 || diagram.data.length == 0} style={ActionButtonStyle} variant="contained" onClick={onBehead}> Behea<u>d</u> </MaterialButton>
+        <MuiThemeProvider theme={theme}>
+          <MaterialButton disabled={!diagram || (target && !diagram.sameBoundary(target))} style={ActionButtonStyle} variant="contained" onClick={onSource}> <u>S</u>ource </MaterialButton>
+          <MaterialButton disabled={!diagram || (source && !diagram.sameBoundary(source))} style={ActionButtonStyle} variant="contained" onClick={onTarget}> <u>T</u>arget </MaterialButton>
+          <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onIdentity}> <u>I</u>dentity </MaterialButton>
+          <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onClear}> <u>C</u>lear </MaterialButton>
+          <MaterialButton disabled={!diagram || slice.length == 0} style={ActionButtonStyle} variant="contained" onClick={onRestrict}> <u>R</u>estrict </MaterialButton>
+          <MaterialButton disabled={!diagram} style={ActionButtonStyle} variant="contained" onClick={onTheorem}> T<u>h</u>eorem </MaterialButton>
+          <MaterialButton disabled={!diagram || diagram.n == 0 || diagram.data.length == 0} style={ActionButtonStyle} variant="contained" onClick={onBehead}> Behea<u>d</u> </MaterialButton>
+        </MuiThemeProvider>
       </div>
     </Tool>
   );
@@ -39,7 +48,8 @@ export default connect(
   state => ({
     source: getSource(state.proof),
     target: getTarget(state.proof),
-    diagram: getDiagram(state.proof)
+    diagram: getDiagram(state.proof),
+    slice: getSlice(state.proof)
   }),
   dispatch => ({
     onSource: () => dispatch({ type: 'workspace/set-source' }),
